7f0772641d394654c7c404c09f05e1f899e7e99d
Aditi Agrawal <39885830+Adii265@users.noreply.github.com>
2019-07-25 00:27:05 +0530

    Update BinaryTree.py

--- BinaryTree/BinaryTree.py
+++ BinaryTree/BinaryTree.py
@@ -46,7 +46,8 @@ class Node:
 	def LevelOrderTraversal(self,Node):
 		h = self.FindHeight(Node)
 		for i in range(1,h+1):
-			self.LevelOrderCall(Node,i)
+			a = self.LevelOrderCall(Node,i)
+		return a
 	def LevelOrderCall(self,Node,level):
 		if Node == None:
 			return 
@@ -54,7 +55,8 @@ class Node:
 			levelorder.append(Node.data)
 		else:
 			self.LevelOrderCall(Node.left,level-1)
-			self.LevelOrderCall(Node.right,level-1)			
+			self.LevelOrderCall(Node.right,level-1)		
+		return levelorder	
 	
 	def FindMaxElement(self,Node):
 		if not(Node):
@@ -80,11 +82,89 @@ class Node:
 			return(max(self.FindHeight(Node.left),self.FindHeight(Node.right))+1)
 	
 	def FindSize(self,Node):
-		pass
+		if Node == None:
+			return 0
+		else:
+			return(self.FindSize(Node.left)+self.FindSize(Node.right)+1)		
+
 	def DeleteTree(self,Node):
-		pass
+		if Node == None:
+			return
+		else:
+			self.DeleteTree(Node.left)
+			self.DeleteTree(Node.right)
+			print(Node.data)
 	def deepestNode(self,Node):
-		pass 			
+		if Node == None:
+			return
+		else:
+			a = self.LevelOrderTraversal(Node)
+			return a[-1]
+			
+	def sumofElements(self,Node):
+		if Node == None:
+			return 0
+		else:
+			return sum(inorder)
+		
+	def NoofFullNodes(self,Node,count):
+		
+		if Node == None:
+			return
+		else:
+			if Node.left and Node.right:
+				count = count+1
+				print(Node.data,count)
+				self.NoofFullNodes(Node.left,count)
+				self.NoofFullNodes(Node.right,count)
+			return count
+		
+
+	def NoofHalfNodes(Self,Node):
+		pass
+
+	def StructurallyIdentical(self,Node1,Node2):
+		pass
+
+	def Diameter(self,Node):
+		pass
+
+	def LevelWithMaxSum(self,Node):
+		pass
+
+	def RoottoLeafPath(self,Node):
+		pass
+
+	#Check if a path exist for given sum
+	def  pathwithSum(self,Node):
+		pass
+	
+	def Mirror(self,Node):
+		pass
+	
+	def CheckMirror(self,Node1,Node2):
+		pass
+
+	def LCA(self,Node,e1,e2):
+		pass  
+		
+	#Create a binary tree using preorder traversal and inorder traversal
+	def InPre(self,Node,i,p):
+		pass
+	
+	def AllAncestors(self,Node,e):
+		pass
+	
+	def ZigZagTraversal(self,Node):
+		pass
+
+	def VerticalSum(self,Node):
+		pass
+	
+	
+
+
+
 
 root = Node(27)
 root.left = Node(2) 
@@ -98,7 +178,7 @@ root.right.left.left = Node(6)
 root.right.left.right = Node(7)
 root.right.right.left = Node(9)
 root.right.right.right = Node(10)
-
+'''
 print("Inorder")
 root.InorderTraversal(root)
 print(inorder)
@@ -112,11 +192,10 @@ root.PostOrderTraversal(root)
 print(postorder)
 
 print("LevelOrder")
-root.LevelOrderTraversal(root)
-print(levelorder)
+print(root.LevelOrderTraversal(root))
 
-print("Max Element of the tree")
-print(root.FindMaxElement(root))
+print("Max Element of the tree ",root.FindMaxElement(root))
+
 
 ele = int(raw_input("Enter element to be searched"))
 if root.SearchAnElement(root,ele) == 1:
@@ -124,8 +203,27 @@ if root.SearchAnElement(root,ele) == 1:
 else:
 	print("Not Found")
 
-print("Height of the tree is")
-print(root.FindHeight(root))
+print("Height of the tree is ",root.FindHeight(root))
+
+print("Size of the Binary Tree ",root.FindSize(root))
 
 
+print("Deepest Node ",root.deepestNode(root))
+
+print("Sum of elements",root.sumofElements(root))
+'''
+
+print("Number of FullNodes", root.NoofFullNodes(root,0))
+
+
+
+
+
+
+
+
+'''
+root.DeleteTree(root)
+root = None
+print("Tree deleted")'''
 
